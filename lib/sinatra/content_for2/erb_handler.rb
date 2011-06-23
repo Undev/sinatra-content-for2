2a6168c5172361c08a7673c7b43acaf002acf7fc
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-06-23 17:24:37 +0400

    New implementation that heavily inspired by padrino

---
+++ lib/sinatra/content_for2/erb_handler.rb
@@ -0,0 +1,37 @@
+class Sinatra::ContentFor2::ErbHandler < Sinatra::ContentFor2::BaseHandler
+  attr_reader :output_buffer
+
+  def initialize(template)
+    super
+    @output_buffer = template.instance_variable_get(:@_out_buf)
+  end
+
+  def is_type?
+    ! self.output_buffer.nil?
+  end
+
+  def block_is_type?(block)
+    is_type? || (block && eval('defined?(__in_erb_template)', block.binding))
+  end
+
+  def engines
+    @engines ||= [ :erb, :erubis ]
+  end
+
+  def capture_from_template(*args, &block)
+    self.output_buffer, _buf_was = '', self.output_buffer
+    block.call(*args)
+    ret = eval('@_out_buf', block.binding)
+    self.output_buffer = _buf_was
+    ret
+  end
+
+protected
+  def output_buffer=(value)
+    template.instance_variable_set(:@_out_buf, value)
+  end
+
+end
+
+Sinatra::ContentFor2::BaseHandler.register(Sinatra::ContentFor2::ErbHandler)
+
